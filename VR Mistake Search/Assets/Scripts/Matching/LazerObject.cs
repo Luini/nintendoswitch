9e100a8895698478ad79b05ca709827682c6b7ed
DESKTOP-F3M385U\TWレンタル <ihsuyadihcu@gmail.com>
2017-09-16 16:31:50 +0900

    データの同期

---
+++ VR Mistake Search/Assets/Scripts/Matching/LazerObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerObject : Photon.MonoBehaviour
+{
+
+    public bool isParent = false;
+    public string targetObject;
+
+	// Use this for initialization
+	void Start () {
+        if (!isParent) ChangeColor();
+
+    }
+
+    void ChangeColor () {
+        LineRenderer line = GetComponent<LineRenderer>();
+        line.startColor = Color.blue;
+        line.endColor = Color.blue;
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(isParent);
+            stream.SendNext(targetObject);
+        }
+        else
+        {
+            isParent = (bool)stream.ReceiveNext();
+            targetObject = (string)stream.ReceiveNext();
+        }
+    }
+}
